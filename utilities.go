b2b3bb07989f56190e2e218a9277ea2fb75b5ce6
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2020-04-23 18:44:12 -0500

    Support all status messages
    This adds better explanations of the various status that can be received
    from the verify endpoint so that users can determine what the reason is
    for their error if they do get one.

--- utilities.go
+++ utilities.go
@@ -27,7 +27,6 @@ func InvalidateToken(ctx context.Context, token string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(requestURI)
 
 	response, err := makeRequest(ctx, requestURI)
 	if err != nil {
@@ -54,12 +53,33 @@ func InvalidateToken(ctx context.Context, token string) error {
 		}
 	}
 
-	if parsedResponse["status"] != "OK" {
+	errorFmt := "failed to invalidate the token: %s (%v)"
+	// https://developers.yubico.com/yubikey-val/Validation_Protocol_V2.0.html
+	switch parsedResponse["status"] {
+	case "OK":
+		return nil
+	case "BAD_OTP":
+		return fmt.Errorf(errorFmt, "The OTP is in an invalid format", parsedResponse)
+	case "REPLAYED_OTP":
+		return fmt.Errorf(errorFmt, "The token has already been seen", parsedResponse)
+	case "BAD_SIGNATURE":
+		return fmt.Errorf(errorFmt, "The HMAC signature verification failed", parsedResponse)
+	case "MISSING_PARAMETER":
+		return fmt.Errorf(errorFmt, "The request is missing a parameter", parsedResponse)
+	case "NO_SUCH_CLIENT":
+		return fmt.Errorf(errorFmt, "The request id does not exist", parsedResponse)
+	case "OPERATION_NOT_ALLOWED":
+		return fmt.Errorf(errorFmt, "The request id is not allowed to verify OTPs", parsedResponse)
+	case "BACKEND_ERROR":
+		return fmt.Errorf(errorFmt, "Unexpected server error", parsedResponse)
+	case "NOT_ENOUGH_ANSWERS":
+		return fmt.Errorf(errorFmt, "Server could not get requested number of syncs during before timeout", parsedResponse)
+	case "REPLAYED_REQUEST":
+		return fmt.Errorf(errorFmt, "Server has seen the OTP/Nonce combination before", parsedResponse)
+	default:
 		return fmt.Errorf("Yubico API did not successfully invalidate token: %v", parsedResponse)
 	}
 	// TODO: Decide if we want to do something with the other parameters
-
-	return nil
 }
 
 func buildURL(token string) (string, error) {
